5f2063ddbc61748459b8eb257deb80813da95e6d
YurleySolimer <yurley.solimer@gmail.com>
2019-02-02 19:19:46 -0400

    Agregando clases

--- AplicacionProducto/app.js
+++ AplicacionProducto/app.js
@@ -1,1 +1,21 @@
-alert("holi");
+class Product {
+	constructor (name, price, year) {
+		this.name = name;
+		this.price = price;
+		this.year = year;
+	}
+}
+
+class UI {
+	addProduct () {
+
+	}
+
+	deleteProduct () {
+
+	}
+
+	showMessage () {
+		
+	}
+}
