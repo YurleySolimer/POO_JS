d32263f3d3fac2312b5647d3c60ca2e5b2a4b914
YurleySolimer <yurley.solimer@gmail.com>
2019-02-02 22:11:14 -0400

    agregando otras funciones

--- AplicacionProducto/app.js
+++ AplicacionProducto/app.js
@@ -27,13 +27,26 @@ class UI {
         productList.appendChild(element);
     }
 
-	deleteProduct () {
-
+	deleteProduct (element) {
+		if (element.name == "delete") {
+			element.parentElement.parentElement.parentElement.remove();
+		}
 	}
 
-	showMessage () {
-
-	}
+	showMessage (message, cssClass) {
+		const div = document.createElement('div');
+        div.className = `alert alert-${cssClass} mt-3`;
+        div.appendChild(document.createTextNode(message));
+        // Show in The DOM
+        const container = document.querySelector('.container');
+        const app = document.querySelector('#App');
+        // Insert Message in the UI
+        container.insertBefore(div, app);
+        // Remove the Message after 3 seconds
+        setTimeout(function () {
+            document.querySelector('.alert').remove();
+        }, 3000);
+    }
 }
 
 //DOM EVENTS
@@ -46,6 +59,12 @@ document.getElementById("product-form").addEventListener("submit", function (e) 
 	const ui = new UI ();
 	ui.addProduct (product);
 	ui.resetForm (); 
+	ui.showMessage("Product added", "success");
 
 	e.preventDefault();
+});
+
+document.getElementById("product-list").addEventListener("click", function (e) {
+	const ui = new UI ();
+	ui.deleteProduct (e.target);
 });
