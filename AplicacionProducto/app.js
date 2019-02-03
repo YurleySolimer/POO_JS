796b552ced1c2d49e3087b9a884cab9c9b6167d1
YurleySolimer <yurley.solimer@gmail.com>
2019-02-02 21:26:06 -0400

    reseteando formulario

--- AplicacionProducto/app.js
+++ AplicacionProducto/app.js
@@ -7,7 +7,11 @@ class Product {
 }
 
 class UI {
-	addProduct(product) {
+	resetForm () {
+		document.getElementById("product-form").reset();
+	}
+
+	addProduct (product) {
         const productList = document.getElementById('product-list');
         const element = document.createElement('div');
         element.innerHTML = `
@@ -38,8 +42,10 @@ document.getElementById("product-form").addEventListener("submit", function (e) 
 	const price = document.getElementById("price").value;
 	const year = document.getElementById("year").value;
 
-	const product = new Product(name, price,year);
+	const product = new Product (name, price,year);
 	const ui = new UI ();
-	ui.addProduct(product);
+	ui.addProduct (product);
+	ui.resetForm (); 
+
 	e.preventDefault();
 });
