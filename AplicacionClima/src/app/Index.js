ebf933ae04e4943fab39a5f97b6ae859ef83f1ab
YurleySolimer <yurley.solimer@gmail.com>
2019-02-06 22:44:41 -0400

    agregando Storage

--- AplicacionClima/src/app/Index.js
+++ AplicacionClima/src/app/Index.js
@@ -1,14 +1,29 @@
 const { Clima } = require('./Clima');
-const { UI } = require('./UI');;
+const { UI } = require('./UI');
+const { Store } = require('./Store');
 
-const clima = new Clima("London", "uk");
-require("./index.css");
+const storage = new Store();
+const {city, countryCode}= storage.getLocationData();
+const clima = new Clima(city, countryCode);
 
 const ui = new UI();
+
+require("./index.css");
 
 async function fetchWeather () {
 	const data = await clima.getWeather ();
 	ui.render(data);
 }
+
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+  const city = document.getElementById('city').value;
+  const countryCode = document.getElementById('countryCode').value
+  clima.changeLocation(city, countryCode);
+  storage.setLocationData(city, countryCode);
+  fetchWeather();
+  e.preventDefault(); 
+
+});
+
 
 document.addEventListener("DOMContentLoaded", fetchWeather());
