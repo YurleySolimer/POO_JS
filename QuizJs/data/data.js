772a6ee0ce86ae2f423dac7dbfacacf8614b6442
yurley solimer <yurley.solimer@gmail.com>
2022-02-12 20:48:36 -0400

    Add QuizJs project

---
+++ QuizJs/data/data.js
@@ -0,0 +1,32 @@
+export const data = [
+  {
+    question: "Cual de las siguientes opciones no es un editor de codigo?",
+    choices: ["vim", "vscode", "sublime", "word"],
+    answer: 'word',
+  },
+  {
+    question: "Que lenguaje no es orientado a objetos",
+    choices: ["python", "javascript", "haskell", "c++"],
+    answer: 'haskell',
+  },
+  {
+    question: "Que lenguaje no sirve para estilizar sitios web",
+    choices: ["stylus", "less", "postCSS", "sass"],
+    answer: 'postCSS',
+  },
+  {
+    question: "Que lenguaje no sirve para estilizar sitios web",
+    choices: ["stylus", "less", "postCSS", "sass"],
+    answer: 'postCSS',
+  },
+  {
+    question: "Que es un algoritomo?",
+    choices: ["Un dibujo que representa un flujo de datos", "El valor que se obtiene de la información", "Un conjunto ordenado de operaciones sistemáticas que permite hacer un cálculo y hallar la solución", "Ninguna de las anteriores"],
+    answer: 'Un conjunto ordenado de operaciones sistemáticas que permite hacer un cálculo y hallar la solución',
+  },
+  {
+    question: "Donde se coloca la llamada de JavaScript en HTML?",
+    choices: ["La sección Body de la página", " Antes de la etiqueta HTML", "Puede colocarse en la sección Head o en Body", "Ninguna de las anteriores"],
+    answer: 'Puede colocarse en la sección Head o en Body',
+  },
+];
---
+++ QuizJs/data/questions.js
@@ -0,0 +1,7 @@
+import { Question } from "../models/Questions.js";
+import { data } from "./data.js";
+
+export const questions = data.map(
+  (question) =>
+    new Question(question.question, question.choices, question.answer)
+);
